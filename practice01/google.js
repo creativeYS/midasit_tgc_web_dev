678294621851b8c8c28fd5c7f9351dd0b817af6a
ssj0324 <ssj0324@midasit.com>
2024-03-19 18:02:50 +0900

    feat : Google Wnd 제작
    - html 코드 적용
    - css 코드 적용
    - js 동작 설정. search, redirect, alert

--- practice01/google.js
+++ practice01/google.js
@@ -1,4 +1,22 @@
 function onSearch() {
+    var searchText = document.getElementById("text_box").value;
+    window.location.href='http://google.com/search?q=' + searchText;
+}
+
+function onClickedGoogleImg() {
+    location.href = location.href;
+}
+
+function onClickedGmail() {
+    window.location.href='https://mail.google.com';
+}
+
+function onClickedImages() {
+    window.location.href='https://images.google.com';
+}
+
+function onLucky() {
+    alert('Lucky!');
 }
 
 window.onload = function () {
