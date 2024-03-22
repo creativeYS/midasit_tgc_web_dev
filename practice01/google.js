97428d8e5eac34c093e97eb4ff44ed843c8cee8a
KimMinseok <kms0523@midasit.com>
2024-03-22 18:21:48 +0900

    window.onload의 소중함..

--- practice01/google.js
+++ practice01/google.js
@@ -1,13 +1,17 @@
+window.onload = function()
+{
+  document.getElementById('button1').onclick = on_search;
+  document.getElementById('button2').onclick = not_yet_developed;
+
+  var HeaderElements =  document.querySelectorAll('.HeaderBox');
+  for (const Element of HeaderElements){
+    Element.addEventListener("click", not_yet_developed);
+  }
+};
+
 function not_yet_developed(){
   alert("not yet developed ")
 }
-
-// var gmailElement = document.getElementById("gmail");
-var gmailElement = document.querySelector(".HeaderBox#gmail");
-gmailElement.addEventListener("click", function() {
-  // 클릭했을 때 실행될 함수 내용을 작성합니다.
-  alert("Gmail을 클릭했습니다!");
-});
 
 function on_search() {
   var input_val = document.getElementById("SearchBox").value;
@@ -21,5 +25,3 @@ function is_enter(event) {
   if (event.keyCode === 13) on_search();
 }
 
-window.onload = function () {
-}
