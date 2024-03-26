5277bef83e308fca4200e55427278531ac7b47df
lhy0118 <lhy0118@midasit.com>
2024-03-26 19:05:12 +0900

    각 버튼 동작 기능 추가
    search 버튼 : input값을 받아와서 google에 검색하도록 구현
    lucky 버튼 : 벚꽃 이미지 링크로 리다이렉트
    Gmail nav : google gmail 페이지로 이동
    Image nav : google image 검색 페이지로 이동
    
    + Enter 입력 시 Key 이벤트를 input에 붙여서 search 버튼과 동일한 동작을 하도록 구현

--- practice01/google.js
+++ practice01/google.js
@@ -1,5 +1,26 @@
-function onSearch() {
+function onClickSearch() {
+	const searchInput = document.getElementById('input').value;
+	const searchURL = 'https://www.google.com/search?q=' + searchInput;
+	window.location.href = searchURL;
+}
+
+function onClickLucky() {
+	window.location.href = 'https://images.pexels.com/photos/1043458/pexels-photo-1043458.jpeg';
+}
+
+function onClickGmail() {
+	window.location.href = 'https://mail.google.com/';
+}
+
+function onClickImage() {
+	window.location.href = 'https://www.google.com/imghp';
 }
 
 window.onload = function () {
+	// Enter 키 입력 이벤트
+	document.getElementById("input").addEventListener("keyup", function(event) {
+		if (event.key === "Enter") {
+			onClickSearch();
+		}
+	});
 }
