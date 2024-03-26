5b976c7b98d545fd58980dbf1f1d205c5fcc2a6a
박찬석 <pcs0807@midasit.com>
2024-03-26 19:47:23 +0900

    <추가> Google 메인화면 만들기 과제

--- practice01/google.js
+++ practice01/google.js
@@ -1,5 +1,28 @@
 function onSearch() {
+    var search = document.getElementById('SearchString').value;
+
+    if (search) 
+    {
+        window.location.href = 'https://www.google.com/search?q=' + search;
+    }
+    else
+    {
+        alert('검색어를 입력해주세요.');
+        return;
+    }
 }
 
-window.onload = function () {
+function handleKeyPress(event) {
+
+    if (event.keyCode === 13) 
+    {
+        onSearch();
+    }
+}
+
+function onClickLucky()
+{
+    var search = document.getElementById('SearchString').value;
+
+    window.location.href = 'https://doodles.google';
 }
