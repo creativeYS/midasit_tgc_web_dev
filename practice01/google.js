b8f3c28f8982a2a45f353c1cc9a08206a6103877
dykwak <dykwak@midasit.com>
2024-03-27 16:24:39 +0900

    createYS/midasit_tgc_wdb_dev:dykwak

--- practice01/google.js
+++ practice01/google.js
@@ -1,5 +1,32 @@
 function onSearch() {
+    var search = document.getElementById('SearchString').value;
+
+    if (search) 
+    {
+        window.location.href = 'https://www.google.com/search?q=' + search;
+    }
+    else
+    {
+        alert('Please enter a search string');
+        return;
+    }
+}
+
+function onClickLucky()
+{
+    var search = document.getElementById('SearchString').value;
+
+    alert('Lucky!');
+}
+
+function handleKeyPress(event) {
+
+    if (event.keyCode === 13) 
+    {
+        onSearch();
+    }
 }
 
 window.onload = function () {
+    onSearch();
 }
