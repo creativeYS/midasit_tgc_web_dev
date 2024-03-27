b66062975b6d6d48f0f1e9066e01a0ad9fa159ac
lhy0118 <lhy0118@midasit.com>
2024-03-27 10:14:58 +0900

    html과 js의 역할에 따라 코드 수정(이벤트 분리 및 태그 수정)
    HTMl에서 onClick이벤트 제거 후, js에서 ID 기반으로 click 이벤트 추가
    div 태그에 onClick이벤트로 새로운 페이지 이동하는 로직을 a 태그로 대체.

--- practice01/google.js
+++ practice01/google.js
@@ -8,19 +8,15 @@ function onClickLucky() {
 	window.location.href = 'https://images.pexels.com/photos/1043458/pexels-photo-1043458.jpeg';
 }
 
-function onClickGmail() {
-	window.location.href = 'https://mail.google.com/';
-}
-
-function onClickImage() {
-	window.location.href = 'https://www.google.com/imghp';
-}
-
 window.onload = function () {
+	// Google Search 버튼 클릭 이벤트
+	document.getElementById("link_search").addEventListener("click", onClickSearch);
 	// Enter 키 입력 이벤트
 	document.getElementById("input").addEventListener("keyup", function(event) {
 		if (event.key === "Enter") {
 			onClickSearch();
 		}
 	});
+
+	document.getElementById("link_lucky").addEventListener("click", onClickLucky);
 }
