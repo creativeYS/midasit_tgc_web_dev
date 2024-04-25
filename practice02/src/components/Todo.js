dc46e9f71e4b08bfc48e8397d219d5b537fcf271
dykwak <dykwak@midasit.com>
2024-04-25 17:41:06 +0900

    체크박스 상태 변경 반영되도록 수정

--- practice02/src/components/Todo.js
+++ practice02/src/components/Todo.js
@@ -1,14 +1,22 @@
+import React, { useState } from 'react';
 import './Todo.css';
 
 function Todo({todo}) {
+
+    const [completed, setCompleted] = useState(todo.completed); // 상태 설정
+
+    const handleCheckboxChange = () => {
+        setCompleted(!completed); // 상태 변경
+    };
+
     const id = todo.id;
     const todoDesc = todo.todo;
-    const completed = todo.completed;
+    //const todocompleted = todo.completed;
 
     return (
         <li key={id} className={completed ? "todoListItem done" : "todoListItem"}>
             <label>
-                <input type="checkbox" className="checkbox" checked={completed} />
+                <input type="checkbox" className="checkbox" checked={completed} onChange={handleCheckboxChange}/>
                 {todoDesc}
             </label>
         </li>
