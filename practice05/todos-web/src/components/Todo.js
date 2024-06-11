07567992a1af5e05413f4d265886e9c4808997cb
syc1214 <syc1214@midasit.com>
2024-06-11 15:52:50 +0900

    프론트 투두 설명 UI 변경.

--- practice05/todos-web/src/components/Todo.js
+++ practice05/todos-web/src/components/Todo.js
@@ -1,24 +1,27 @@
 import './Todo.css';
 
 function Todo({todo, setTodo, deleteTodo}) {
-    const {id, content, done} = todo;
+    const {id, content, description, done} = todo;
 
     const onClickDelete = () => {
         deleteTodo(id);
     }
 
     return (
-        <li key={id} className={done ? "todoListItem done" : "todoListItem"}>
-            <label>
-                <input type="checkbox"
-                       className="checkbox"
-                       checked={done}
-                       onClick={()=>setTodo({...todo, done:!done})}
-                />
-                {content}
-            </label>
-            <button className = "TodoButtonInside" onClick={onClickDelete}>제거</button>
-        </li>
+            <li key={id} className={done ? "todoListItem done" : "todoListItem"}>
+                <div className="contentSection">
+                    <label>
+                        <input type="checkbox"
+                            className="checkbox"
+                            checked={done}
+                            onClick={() => setTodo({...todo, done: !done})}
+                        />
+                        {content}
+                    </label>
+                    <button className="TodoButtonInside" onClick={onClickDelete}>제거</button>
+                </div>
+                <div className="description">{description}</div>
+            </li>
     );
 }
 
